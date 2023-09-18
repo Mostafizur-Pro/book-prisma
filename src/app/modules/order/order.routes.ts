212b25589db632086888b37f16ef83183bae4a3e
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-09-18 22:49:35 +0600

    order routes add

---
+++ src/app/modules/order/order.routes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { OrderController } from './order.controller';
+const router = express.Router();
+
+router.get('/', 
+
+OrderController.getAllOrders);
+router.get('/:id', 
+
+OrderController.getOrderById);
+router.post(
+  '/create-order',
+
+  OrderController.createOrder
+);
+
+export const orderRoutes = router;
