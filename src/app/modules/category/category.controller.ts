d0fdc2213ffc10080b42e2cd2110dbba1d5a57b6
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-09-18 22:17:09 +0600

    category all route successfully work

--- src/app/modules/category/category.controller.ts
+++ src/app/modules/category/category.controller.ts
@@ -18,7 +18,70 @@ const createCategory = catchAsync(async (req:Request, res: Response):Promise<Cat
     return category;
 });
 
-export const CategoryController = {
 
-    createCategory
+const getAllCategories = catchAsync(async (req:Request, res: Response):Promise<Category[]> => {
+    const categories = await CategoryService.getAllCategories();
+    sendResponse( res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Categories fetched successfully',
+        data: categories
+    });
+
+    return categories;
+});
+
+const getCategoryById = catchAsync(async (req:Request, res: Response):Promise<Category> => {
+    const categoryId = req.params.id;
+    const category = await CategoryService.getCategoryById(categoryId);
+
+    sendResponse( res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Category fetched successfully',
+        data: category
+    });
+
+    return category;
+});
+
+
+
+const updateCategoryById = catchAsync(async (req:Request, res: Response):Promise<Category> => {
+    const categoryId = req.params.id;
+    const payload = req.body;
+
+    const category = await CategoryService.updateCategoryById(categoryId, payload);
+
+    sendResponse( res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Category updated successfully',
+        data: category
+    });
+
+    return category;
+});
+
+const deleteCategoryById = catchAsync(async (req:Request, res: Response):Promise<Category> => {
+    const categoryId = req.params.id;
+    const category = await CategoryService.deleteCategoryById(categoryId);
+
+    sendResponse( res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Category deleted successfully',
+        data: category
+    });
+
+    return category;
+});
+
+export const CategoryController = {
+    createCategory,
+
+    getAllCategories,
+    getCategoryById,
+    updateCategoryById,
+    deleteCategoryById
 };
--- src/app/modules/category/category.routes.ts
+++ src/app/modules/category/category.routes.ts
@@ -3,7 +3,19 @@ import { CategoryController } from './category.controller';
 
 const router = express.Router();
 router.post(
-    '/create-category', CategoryController.createCategory  );
+    '/create-category', CategoryController.createCategory);
+router.get('/', CategoryController.getAllCategories);
+router.get('/:id', CategoryController.getCategoryById);
 
 
-  export const categoryRoutes = router;
+router.patch(
+    '/:id',
+
+    CategoryController.updateCategoryById
+);
+router.delete(
+    '/:id',
+    CategoryController.deleteCategoryById
+);
+
+export const categoryRoutes = router;
