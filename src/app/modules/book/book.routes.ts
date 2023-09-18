b8eb44c3d739b70e9f7f853b04c25e9edf4d2106
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-09-18 22:54:16 +0600

    book validation add

--- src/app/modules/book/book.routes.ts
+++ src/app/modules/book/book.routes.ts
@@ -1,14 +1,20 @@
 
 import express from 'express';
 import { BookController } from './book.controller';
+import ValidateRequest from '../../middleware/validateRequest';
+import { BookValidation } from './book.zod.validation';
 
 
 const router = express.Router();
 router.get('/', BookController.getAllBooks);
 router.get('/:id', BookController.getBookByCategoryId);
 router.get('/:id', BookController.getBookById);
-router.post("/create-book", BookController.createBook);
-router.patch("/:id", BookController.updateBookById);
+router.post("/create-book", 
+ValidateRequest( BookValidation.postValidation ),
+BookController.createBook);
+router.patch("/:id", 
+ValidateRequest( BookValidation.postValidation ),
+BookController.updateBookById);
 router.delete("/:id", BookController.deleteBookById);
 
 export const bookRoutes = router;
