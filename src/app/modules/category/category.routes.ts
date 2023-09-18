60e421dea1fd5916362626e512802a5fd05822ae
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-09-18 22:55:52 +0600

    category validation add

--- src/app/modules/category/category.routes.ts
+++ src/app/modules/category/category.routes.ts
@@ -1,20 +1,26 @@
 import express from 'express';
 import { CategoryController } from './category.controller';
+import ValidateRequest from '../../middleware/validateRequest';
+import { CategoryValidation } from './category.zod.validation';
 
 const router = express.Router();
 router.post(
-    '/create-category', CategoryController.createCategory);
+    '/create-category',
+    ValidateRequest(CategoryValidation.zodValidation),
+     CategoryController.createCategory);
 router.get('/', CategoryController.getAllCategories);
 router.get('/:id', CategoryController.getCategoryById);
 
 
 router.patch(
     '/:id',
+    ValidateRequest(CategoryValidation.zodValidation),
 
     CategoryController.updateCategoryById
 );
 router.delete(
     '/:id',
+    ValidateRequest(CategoryValidation.zodValidation),
     CategoryController.deleteCategoryById
 );
 
